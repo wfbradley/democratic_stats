4fe0cfc81c177f8ab5d2b29cf8e82e07dea31f6f
Bill Bradley <wfbradley@cmtelematics.com>
2016-01-12 10:56:58 -0500

    Refactored more sanely to support arbitrary numbers of curves (by doing one at a time); computing probability of one coin exceeding the other, or exceeding the other by "a lot"

--- comparing_rates.py
+++ comparing_rates.py
@@ -7,68 +7,52 @@ import sys
 import argparse
 
 
-def plot_two_betas(a1=None,b1=None,label1=None,a2=None,b2=None,label2=None,num=10000,threshold=.001):
-    x=np.linspace(1./float(num+1),1.0-1./float(num+1),num=num)
-    y1=beta.pdf(x,a1,b1)
-    big1=y1>=threshold
-    plt.plot(100.0*x[big1],y1[big1],label=label1)
-    if not (a2 is None):
-        y2=beta.pdf(x,a2,b2)
-        big2=y2>=threshold
-        plt.plot(100.0*x[big2],y2[big2],label=label2)
-    if not(label1 is None) or not(label2 is None):
+def plot_two_betas(a=None,b=None,label=None,num=600,threshold=.001):
+    epsilon=min(0.1/(a+b),.000001)
+
+    if False: # Use uniform points
+        x=np.linspace(1./float(num+1),1.0-1./float(num+1),num=num)
+    else: # intelligently sample around modes
+        var_x=a*b/( np.square(a+b)*(a+b+1.0) )
+        std_dev_x=np.sqrt(var_x)
+        mode_x=(a-1.0)/(a+b-2.0)
+        mm=max(0.0,mode_x-6.0*std_dev_x)
+        MM=min(1.0,mode_x+6.0*std_dev_x)
+        x=np.linspace(mm+epsilon,MM-epsilon,num=num)
+    y=beta.pdf(x,a,b)
+    big=y>=threshold
+    plt.plot(100.0*x[big],y[big],label=label)
+    if not(label is None):
         plt.legend(loc='best')
     plt.xlabel('Rate (in %)')
-    plt.ylabel('Likelihood')
+    plt.ylabel('Probability density')
+    return((x,y))
 
-def plot_two_samples(a1=None,n1=None,label1='',a2=None,n2=None,label2='',num=10000,threshold=.001):
-    # In case a1, etc are passed as strings instead of floats
-    a1=float(a1)
-    n1=float(n1)
+def plot_event_likelihood(a=None,n=None,label='',num=600,threshold=.001):
+    # In case a, etc are passed as strings instead of floats
+    a=float(a)
+    n=float(n)
 
-    b1=n1-a1
-    if a1<0 or n1<a1:
-        print "Must have 0<=a1<=n1"
+    b=n-a
+    if a<0 or n<a:
+        print "Must have 0<=a<=n"
         sys.exit(0)
 
-    label1_extra='%d of %d (%.2f%%)'%(int(a1),int(n1),100.0*a1/n1)
-    if label1 is '':
-        label1=label1_extra
+    label_extra='%d of %d (%.2f%%)'%(int(a),int(n),100.0*a/n)
+    if label is '':
+        label=label_extra
     else:
-        label1+=' (%s)'%label1_extra
-
-
-
-    # Must have a2 and n2 BOTH none or NEITHER none:
-    if (a2 is np.nan) or (n2 is np.nan):
-        if not ((a2 is np.nan) and (n2 is np.nan)):
-            print "Need to set a2 AND n2, if using them."
-            sys.exit(0)
-    else:
-        a2=float(a2)
-        n2=float(n2)
-        if a2<0 or n2<a2:
-            print "Must have 0<=a2<=n2"
-            sys.exit(0)
-        b2=n2-a2
-
-        label2_extra='%d of %d (%.2f%%)'%(int(a2),int(n2),100.0*a2/n2)
-        if label2 is '':
-            label2=label2_extra
-        else:
-            label2+=' (%s)'%label2_extra
-
+        label+=' (%s)'%label_extra
     # Add uniform prior:
-    a1+=1
-    b1+=1
-    a2+=1
-    b2+=1
+    a+=1
+    b+=1
 
-    plot_two_betas(a1=a1,b1=b1,a2=a2,b2=b2,label1=label1,label2=label2,threshold=threshold)
+    return(plot_two_betas(a=a,b=b,label=label,num=num,threshold=threshold))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=
-        'Comparing probability between two coins (posterior is beta given uniform prior).  Coin #2 and labels are optional.')
+        'Probability of underlying rate of events based on observations.  '
+        'Can optionally specify observations for a second rate.')
     parser.add_argument('--a1', type=float, required=True,
         help='Heads (coin #1)')
     parser.add_argument('--a2', type=float, default=np.nan,
@@ -83,9 +67,63 @@ if __name__ == "__main__":
         help='Label (coin #2)')
     parser.add_argument('--threshold', type=float, default=.001,
         help='Only graph probability values greater than threshold (to restrict x range)')
+    parser.add_argument('--plot_resolution', type=float, default=600,
+        help='Resolution of plot (e.g. 600=high resolution, 10=low resolution)')
     args = parser.parse_args()
 
+    # Plot first curve
+    (x1,y1)=plot_event_likelihood(a=args.a1,n=args.n1,label=args.label1,
+        num=args.plot_resolution,threshold=args.threshold)
 
-    num_x_axis_samples=10000
-    plot_two_samples(a1=args.a1,n1=args.n1,label1=args.label1,a2=args.a2,n2=args.n2,label2=args.label2,num=num_x_axis_samples,threshold=args.threshold)
+    # If specified, plot second curve
+    if (args.a2 is not np.nan) and (args.n2 is not np.nan):
+        (x2,y2)=plot_event_likelihood(a=args.a2,n=args.n2,label=args.label2,
+            num=args.plot_resolution,threshold=args.threshold)
+
+        # Figure out probability that second rate is > first rate.
+        prob_2_is_bigger=0.0
+        prob_normalizer=0.0
+
+        mean1=args.a1/args.n1
+        mean2=args.a2/args.n2
+        two_bigger=False
+        bigger=1
+        smaller=2
+        if mean2 >= mean1:
+            two_bigger=True
+            print 'two_bigger',two_bigger
+            bigger=2
+            smaller=1
+
+        prob_2_is_much_bigger=0.0
+        much_bigger_threshold=np.abs((mean1-mean2)/2.0)
+        print mean1, mean2, much_bigger_threshold
+        for i in xrange(len(x2)):
+            for j in xrange(len(x1)):
+                p_ij=y2[i]*y1[j]
+                if x2[i]>x1[j]:
+                    prob_2_is_bigger+=p_ij
+                if two_bigger:
+                    #print 'x2[%d]=%f'%(i,x2[i])
+                    #print '[%d,%d], x1=%f, x2=%f >? %f  [%f]'%(i,j,x1[j],x2[i],x1[j]+much_bigger_threshold,much_bigger_threshold)
+                    #print x2[i],x1[j]+much_bigger_threshold,x1[j],much_bigger_threshold
+                    if x2[i]>x1[j]+much_bigger_threshold:
+                        prob_2_is_much_bigger+=p_ij
+                else:
+                    if x1[j]>x2[i]+much_bigger_threshold:
+                        prob_2_is_much_bigger+=p_ij
+
+                prob_normalizer+=p_ij
+        print prob_2_is_bigger
+        print prob_2_is_much_bigger
+        print prob_normalizer
+        prob_2_is_bigger/=prob_normalizer
+        prob_2_is_much_bigger/=prob_normalizer
+        print "There is a %.2f%% chance that Coin #2 is greater than Coin #1"%(
+            100.0*prob_2_is_bigger)
+        print "(so, a %.2f%% chance that Coin #1 is greater than or equal to Coin #1)."%(
+            100.0*(1.0-prob_2_is_bigger))
+        print "The probability that Coin #%d is greater than Coin #%d by at least %.3f%% is %.2f%%."%(
+            bigger,smaller,100.0*much_bigger_threshold,100.0*prob_2_is_much_bigger)
+
     plt.show()
